e011a1ab8b6bfe1db0493d63a0bc8e2fa556f6d5
Hrishikesh Barhate <HrishikeshBarhate@gmail.com>
2022-03-29 22:54:07 +0530

    TTT-3 Commit Model files

--- src/main/java/com/game/service/GameService.java
+++ src/main/java/com/game/service/GameService.java
@@ -1,8 +1,11 @@
 package com.game.service;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
 import com.game.model.Game;
+import com.game.model.GameStatus;
 import com.game.model.Player;
 
 @Service
@@ -11,6 +14,9 @@ public class GameService {
 	public Game createGame(Player player) {
 		Game game = new Game();
 		game.setBoard(new int[3][3]);
+		game.setGameId(UUID.randomUUID().toString());
+		game.setPlayer1(player);
+		game.setGameStatus(GameStatus.NEW);
 		return game;
 	}
 
