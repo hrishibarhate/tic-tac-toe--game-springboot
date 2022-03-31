03c92df23ca7df501adf7c03b2fec7d538e47f14
Hrishikesh.Barhate <Hrishikesh.Barhate@BRILAP4387.BRILLIO.COM>
2022-03-31 21:58:38 +0530

    TTT-5 Commiting new Methods and Exception

--- src/main/java/com/game/service/GameService.java
+++ src/main/java/com/game/service/GameService.java
@@ -6,9 +6,12 @@ import org.springframework.stereotype.Service;
 
 import com.game.exception.InvalidGameException;
 import com.game.exception.InvalidParamException;
+import com.game.exception.NotFoundException;
 import com.game.model.Game;
+import com.game.model.GamePlay;
 import com.game.model.GameStatus;
 import com.game.model.Player;
+import com.game.model.TicToe;
 import com.game.repo.GameStorage;
 
 @Service
@@ -41,6 +44,46 @@ public class GameService {
 		GameStorage.getInstance().setGames(game);
 
 		return game;
+	}
+	
+	public Game connectToRandomGame(Player player2) throws NotFoundException {
+		Game game = GameStorage.getInstance().getGames().values().stream()
+				.filter(it->it.getGameStatus().equals(GameStatus.NEW))
+				.findFirst().orElseThrow( ()-> new NotFoundException("Game not Found"));
+		
+		game.setPlayer2(player2);
+		game.setGameStatus(GameStatus.IN_PROGRESS);
+		GameStorage.getInstance().setGames(game);
+		
+		return game;
+	}
+	
+	public Game gamePlay(GamePlay gamePlay) throws NotFoundException, InvalidGameException {
+		if(!GameStorage.getInstance().getGames().containsKey(gamePlay.getGameId())) {
+			throw new NotFoundException("Game not Found");
+		}
+		Game game = GameStorage.getInstance().getGames().get(gamePlay.getGameId());
+		
+		if(game.getGameStatus().equals(GameStatus.FINISHED)) {
+			throw new InvalidGameException("Game has already finished.");
+		}
+		
+		int [][]board = game.getBoard();
+		board[gamePlay.getCoordinateX()][gamePlay.getCoordinateY()] = gamePlay.getType().getValue();
+		
+		checkWinner(game.getBoard(), TicToe.X);
+		checkWinner(game.getBoard(), TicToe.O);
+		
+		
+		
+		return game;		
+	}
+
+	private Boolean checkWinner(int[][] board, TicToe o) {
+		//first converting 2 dimensional array into 1 dimensional.
+		
+		
+		return null;		
 	}
 
 }
